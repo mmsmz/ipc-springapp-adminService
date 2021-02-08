6f63164f18713887d7e3ed7b76865898d6e134aa
Kavuri Karthikeyan <kavuri15@gmail.com>
2021-02-08 08:15:24 -0800

    updateCourseSchedule

--- src/main/java/com/ipc/adminService/repo/CourseScheduleRepository.java
+++ src/main/java/com/ipc/adminService/repo/CourseScheduleRepository.java
@@ -2,7 +2,7 @@ package com.ipc.adminService.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.ipc.adminService.entity.CoursePriceEntity;
+
 import com.ipc.adminService.entity.CourseScheduleEntity;
 
 public interface CourseScheduleRepository extends JpaRepository<CourseScheduleEntity, String>  {
